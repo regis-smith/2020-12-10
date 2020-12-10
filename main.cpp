c3e4c06fc37045b5c8176ac90a084c9e53121bad
Regis Smith <rsmith@t480.whistlin.com>
2020-12-10 04:06:09 -0800

    Demonstrate a min_bucket with strings

--- main.cpp
+++ main.cpp
@@ -1,23 +1,37 @@
 #include "min_bucket.h"
 #include <iostream>
 #include <random>
+#include <fstream>
+#include <vector>
+#include <string>
 
 int main()
 {
+  // load the dictionary
+  std::ifstream infile("words.txt");
+  if (!infile.is_open()) {
+    std::cout << "Can not open dictionary.\n";
+    throw std::exception();
+  }
+  std::string word;
+  std::vector<std::string> words; // the list of words
+  while (infile >> word) words.push_back(word);
+  infile.close();
+  
+  // Insert some random words into a min_bucket
   std::mt19937 e; // Mersenne Twister engine
-  std::uniform_int_distribution<int> u(1,100);
+  std::uniform_int_distribution<int> u(0,words.size());
 
   // A min_bucket of size 5
-  min_bucket<int> bukit(5);
+  min_bucket<std::string> bukit(5);
   
-  // insert 50 random ints
+  // insert 50 random words
   for (int i=0; i<50; i++) {
-    int x = u(e);
+    std::string x = words[u(e)];
     bukit.insert(x);
     std::cout << x << " was inserted.\n";
   }
 
-  // The bucket should have 1, 4, 10, 11, and 12
   std::cout << "The members of the min_bucket are:\n";
   std::cout << bukit[0] << "\t" << bukit[1] << "\t"
             << bukit[2] << "\t" << bukit[3] << "\t"
